ec8eaad6b290e7581605cec323f0c475503f5827
nndaptriiu <nndaptriiu@gmail.com>
2025-12-11 20:13:45 +0700

    Add files via upload

---
+++ Percobaan1.py
@@ -0,0 +1,4 @@
+print('Silakan masukan dua angka untuk pembagian.')
+num1 = int(input('Masukan angka yang akan dibagi: '))
+num2 = int(input('Masukan angka pembagi: '))
+print('{0} dibagi {1} = {2}'.format(num1, num2, num1/num2))
